b148f6792fd4e838c5b92328a574e12f1f01bc4c
Olz <lzosam651@gmail.com>
2025-11-25 21:58:40 +0100

    UPDATE EXO

--- P1_EX3.py
+++ P1_EX3.py
@@ -1,10 +1,18 @@
 #TYPE DONNEES 
 nom = "Jean"
 age = 34
+taille = 1.85
 est_etudiant = True
 
 #EXO
+print(nom)
+print(age)
+print(taille)
+print(est_etudiant)
 
 print (type(nom))
 print (type(age))
+print (type(taille))
 print  (type(est_etudiant))
+
+ 
