f24c01c994ac9a2ea578a703d36bfe8c8a78b980
Olz <lzosam651@gmail.com>
2025-11-25 21:14:11 +0100

    EX2 PY P1

--- P1_EX2.py
+++ P1_EX2.py
@@ -1,5 +1,15 @@
 #F-Str
-nom = "DUPONT"
-prenom = "Marc"
+Nom = "DUPONT"
+Prenom = "Marc"
 
-print(f"Bonjour, je m'appele {nom} {prenom} et vous? ")
+print(f"Bonjour, je m'appelle {Nom} {Prenom} et vous? ")
+
+
+#EXO
+
+Nom2 = "HUGO"
+Prenom2 = "Victor"
+Age_actuel = 30
+Age_actuel2= Age_actuel + 10
+print(f"Je m'appelle {Nom2} {Prenom2} et j'ai {Age_actuel} ans. ")
+print(f"Je m'appelle {Nom2} {Prenom2} et j'ai {Age_actuel2} maintenant ans. ")
