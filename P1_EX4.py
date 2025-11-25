1bd9994aa5d69d5eede76293608da7a971bfbba6
Olz <lzosam651@gmail.com>
2025-11-25 22:49:31 +0100

    EX4 PY P1

---
+++ P1_EX4.py
@@ -0,0 +1,17 @@
+#EX4 PY P1
+liste_fruit = ["pomme","banane","orange"]
+print(liste_fruit)
+
+liste_fruit.append("kiwi")
+print(liste_fruit)
+
+liste_fruit.remove("orange")
+print(liste_fruit)
+
+liste_fruit[2] = "ananas"
+print(liste_fruit)
+
+print(len(liste_fruit))
+
+liste_fruit.sort()
+print(liste_fruit)
