de68b9fcd57ca47dc82ccb72a0e3aa1dd9af87bb
Olz <lzosam651@gmail.com>
2025-11-27 22:39:02 +0100

    update

--- P1_EX6.py
+++ P1_EX6.py
@@ -1,1 +1,8 @@
-#pratiquer les dictionnaires
+#pratiquer les dictionnaire
+
+mon_dictionnaire_fruit = { "pomme":"rouge","banane":"jaune","orange":"orange"}
+mon_dictionnaire_fruit["kiwi"] = "vert"
+couleur_banane = fruits["banane"] 
+mon_dictionnaire_fruit["pomme"] = "vert"
+del mon_dictionnaire_fruit["banane"]
+print(mon_dictionnaire_fruit.keys())
